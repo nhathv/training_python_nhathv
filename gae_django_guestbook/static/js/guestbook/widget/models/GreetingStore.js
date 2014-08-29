3f0ba6f6869aaa15982f2fb52d6f9415948d9605
nhathv <nhathv@aoi-sys.vn>
2014-08-29 10:10:33 +0700

    Modify for Gadget

--- gae_django_guestbook/static/js/guestbook/widget/models/GreetingStore.js
+++ gae_django_guestbook/static/js/guestbook/widget/models/GreetingStore.js
@@ -21,6 +21,7 @@ define([
                     this.store = new JsonRest({
                         target: url,
                         headers: {
+                            "X-Requested-With": null,
                             "X-CSRFToken": _cookie('csrftoken')
                         }
                     });
