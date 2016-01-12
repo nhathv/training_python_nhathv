0e9ccb422e43f5d89ba178516556e3ecc136314a
nhathv <nhathv@aoi-sys.vn>
2016-01-12 14:07:39 +0700

    fix tab indent

--- gae_django_guestbook/static/js/guestbook/gadget/stubs/gadgets.stub.js
+++ gae_django_guestbook/static/js/guestbook/gadget/stubs/gadgets.stub.js
@@ -10,7 +10,7 @@
  */
 
 var gadgets = this.gadgets = {
-  __global__: this  // only for this stub
+	__global__: this  // only for this stub
 };
 
 /**
@@ -18,92 +18,92 @@ var gadgets = this.gadgets = {
  *
  */
 gadgets.io = function() {
-  return {
-    makeRequest : function (url, callback, opt_params) {
-      // convert gadgets.io request to XHR request.
-      opt_params = opt_params || {};
-      var dfd, xhrArgs = {},
-          method = opt_params[gadgets.io.RequestParameters.METHOD];
-          contentType = opt_params[gadgets.io.RequestParameters.CONTENT_TYPE] || gadgets.io.ContentType.JSON;
+	return {
+		makeRequest : function (url, callback, opt_params) {
+			// convert gadgets.io request to XHR request.
+			opt_params = opt_params || {};
+			var dfd, xhrArgs = {},
+				method = opt_params[gadgets.io.RequestParameters.METHOD];
+			contentType = opt_params[gadgets.io.RequestParameters.CONTENT_TYPE] || gadgets.io.ContentType.JSON;
 
-      if (opt_params[gadgets.io.RequestParameters.HEADERS]) {
-        xhrArgs['headers'] = opt_params[gadgets.io.RequestParameters.HEADERS];
-      }
-      if (opt_params[gadgets.io.RequestParameters.POST_DATA]) {
-        xhrArgs['postData'] = opt_params[gadgets.io.RequestParameters.POST_DATA];
-      }
+			if (opt_params[gadgets.io.RequestParameters.HEADERS]) {
+				xhrArgs['headers'] = opt_params[gadgets.io.RequestParameters.HEADERS];
+			}
+			if (opt_params[gadgets.io.RequestParameters.POST_DATA]) {
+				xhrArgs['postData'] = opt_params[gadgets.io.RequestParameters.POST_DATA];
+			}
 
-      xhrArgs['handleAs'] = contentType.toLowerCase(); // json or text
-      xhrArgs['url'] = url + [
-        (url.indexOf('?') > -1 ? '&' : '?'),
-        'opensocial_owner_id=dummy',
-        '&opensocial_viewer_id=dummy'
-      ].join('');
+			xhrArgs['handleAs'] = contentType.toLowerCase(); // json or text
+			xhrArgs['url'] = url + [
+					(url.indexOf('?') > -1 ? '&' : '?'),
+					'opensocial_owner_id=dummy',
+					'&opensocial_viewer_id=dummy'
+				].join('');
 
-      dfd = dojo.xhr(method, xhrArgs);
-      return dojo.Deferred.when(
-        dfd,
-        function(data) {
-          var xhrObj = dfd.ioArgs.xhr,
-            status = xhrObj.status,
-            responseText = xhrObj.responseText;
-          return callback({
-            'data': data, 'rc': status,
-            'text': responseText, 'errors': []});
-        },
-        function(error) {
-          var res = {
-            'errors': ['Error ' + error.status],  // dummy
-            'rc': error.status,
-            'text': error.responseText
-          };
-          return callback(res);
-        }
-      );
-    },
+			dfd = dojo.xhr(method, xhrArgs);
+			return dojo.Deferred.when(
+				dfd,
+				function(data) {
+					var xhrObj = dfd.ioArgs.xhr,
+						status = xhrObj.status,
+						responseText = xhrObj.responseText;
+					return callback({
+						'data': data, 'rc': status,
+						'text': responseText, 'errors': []});
+				},
+				function(error) {
+					var res = {
+						'errors': ['Error ' + error.status],  // dummy
+						'rc': error.status,
+						'text': error.responseText
+					};
+					return callback(res);
+				}
+			);
+		},
 
-    encodeValues : function (fields) {
-    },
+		encodeValues : function (fields) {
+		},
 
-    getProxyUrl : function (url, opt_params) {
-    }
-  };
+		getProxyUrl : function (url, opt_params) {
+		}
+	};
 }();
 
 gadgets.io.RequestParameters = {
-   METHOD : 'METHOD',
-  CONTENT_TYPE : "CONTENT_TYPE",
-  POST_DATA : "POST_DATA",
-  HEADERS : "HEADERS",
-  AUTHORIZATION : 'AUTHORIZATION',
-  NUM_ENTRIES : 'NUM_ENTRIES',
-  GET_SUMMARIES : 'GET_SUMMARIES',
-  REFRESH_INTERVAL : 'REFRESH_INTERVAL'
+	METHOD : 'METHOD',
+	CONTENT_TYPE : "CONTENT_TYPE",
+	POST_DATA : "POST_DATA",
+	HEADERS : "HEADERS",
+	AUTHORIZATION : 'AUTHORIZATION',
+	NUM_ENTRIES : 'NUM_ENTRIES',
+	GET_SUMMARIES : 'GET_SUMMARIES',
+	REFRESH_INTERVAL : 'REFRESH_INTERVAL'
 };
 
 gadgets.io.MethodType = {
-  GET : 'GET',
-  POST : 'POST',
-  PUT : 'PUT',
-  DELETE : 'DELETE',
-  HEAD : 'HEAD'
+	GET : 'GET',
+	POST : 'POST',
+	PUT : 'PUT',
+	DELETE : 'DELETE',
+	HEAD : 'HEAD'
 };
 
 gadgets.io.ContentType = {
-  TEXT : 'TEXT',
-  DOM : 'DOM',
-  JSON : 'JSON',
-  FEED : 'FEED'
+	TEXT : 'TEXT',
+	DOM : 'DOM',
+	JSON : 'JSON',
+	FEED : 'FEED'
 };
 
 gadgets.io.AuthorizationType = {
-  NONE : 'NONE',
-  SIGNED : 'SIGNED',
-  OAUTH : 'OAUTH'
+	NONE : 'NONE',
+	SIGNED : 'SIGNED',
+	OAUTH : 'OAUTH'
 };
 
 gadgets.io.ProxyUrlRequestParameters = {
-  REFRESH_INTERVAL : 'REFRESH_INTERVAL'
+	REFRESH_INTERVAL : 'REFRESH_INTERVAL'
 };
 
 
@@ -112,14 +112,14 @@ gadgets.io.ProxyUrlRequestParameters = {
  *
  */
 gadgets.util = function() {
-  return {
-    escapeString : function(str) {},
-    getFeatureParameters : function (feature) {},
-    hasFeature : function (feature) {},
-    registerOnLoadHandler : function (callback) { dojo.ready(callback); },
-    unescapeString : function(str) {},
-    sanitizeHtml : function(text) {}
-  };
+	return {
+		escapeString : function(str) {},
+		getFeatureParameters : function (feature) {},
+		hasFeature : function (feature) {},
+		registerOnLoadHandler : function (callback) { dojo.ready(callback); },
+		unescapeString : function(str) {},
+		sanitizeHtml : function(text) {}
+	};
 }();
 
 
@@ -138,6 +138,6 @@ gadgets.Prefs.prototype.setArray = function(key, val) {};
 gadgets.Prefs.prototype.getMsg = function(key) {};
 gadgets.Prefs.prototype.getCountry = function() {};
 gadgets.Prefs.prototype.getLang = function() {
-  return gadgets.__global__.__LANG__;
+	return gadgets.__global__.__LANG__;
 };
 gadgets.Prefs.prototype.getModuleId = function() {};
